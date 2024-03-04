8586a287edcc447f8bee0794ea7e9134dd406aeb
linqi <wkw4399xs@gmail.com>
2024-03-04 15:26:22 +0800

    feature:完成项目请求上下文搭建:响应搭建

---
+++ BlossomGateway-Common/src/main/java/response/DisplayResponse.java
@@ -0,0 +1,22 @@
+package response;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description  Response类
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DisplayResponse {
+    private HttpResponseStatus status;
+    private int code;
+    private String message;
+    private String data;
+}
