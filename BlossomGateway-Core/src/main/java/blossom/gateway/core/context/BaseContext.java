b3c5091787e49502a151ffe38c1f18f4eaf4d9f2
linqi <wkw4399xs@gmail.com>
2024-03-04 20:33:25 +0800

    feature:完成项目请求上下文搭建:基础上下文

---
+++ BlossomGateway-Core/src/main/java/blossom/gateway/core/context/BaseContext.java
@@ -0,0 +1,16 @@
+package blossom.gateway.core.context;
+
+import cn.hutool.core.collection.CollectionUtil;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description BaseContext类
+ */
+
+public class BaseContext implements IContext {
+}
