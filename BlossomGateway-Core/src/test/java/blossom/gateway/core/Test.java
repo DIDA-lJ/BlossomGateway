d2ee0ed3a48a8d6e920ed6f000794a38b879e652
linqi <wkw4399xs@gmail.com>
2024-03-05 21:19:25 +0800

    feature:配置加载器完成:配置加载器完成测试

---
+++ BlossomGateway-Core/src/test/java/blossom/gateway/core/Test.java
@@ -0,0 +1,28 @@
+package blossom.gateway.core;
+
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description
+ */
+
+public class Test {
+    public static final AtomicInteger ai = new AtomicInteger(10);
+
+    public static void main(String[] args) {
+        //System.out.println(ai);
+        //System.out.println(ai.incrementAndGet());
+        //
+        //String protocol = null;
+        //Assert.isNull(protocol, "protocal can not be null!");
+        // new GatewayContext.GatewayContextBuilder().setNettyContext(null)
+        //         .setRule(new Rule())
+        //        .build();
+
+        Properties properties = System.getProperties();
+        System.out.println(properties);
+    }
+}
