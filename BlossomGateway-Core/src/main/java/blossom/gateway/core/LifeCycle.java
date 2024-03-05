e2bc2ffee0cbfcf63e96b6027826440d412048d0
linqi <wkw4399xs@gmail.com>
2024-03-05 21:15:34 +0800

    feature:完成项目请求上下文搭建:LifeCycle接口

---
+++ BlossomGateway-Core/src/main/java/blossom/gateway/core/LifeCycle.java
@@ -0,0 +1,29 @@
+package blossom.gateway.core;
+
+/**
+ * @InterfaceName LifeCycle
+ * @Description LifeCycle接口
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2024/03/05
+ */
+public interface LifeCycle {
+
+    /**
+     * 初始化
+     */
+    void init();
+
+    /**
+     * 启动
+     */
+    void start();
+
+
+    /**
+     * 关闭
+     */
+    void shutdown();
+
+
+}
