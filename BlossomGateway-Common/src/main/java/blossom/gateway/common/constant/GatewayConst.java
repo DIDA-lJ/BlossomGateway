d98024a64444fac07430394fc7278f512773cae8
linqi <wkw4399xs@gmail.com>
2024-03-11 15:51:56 +0800

    feature:网关常量类

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/constant/GatewayConst.java
@@ -0,0 +1,17 @@
+package blossom.gateway.common.constant;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 网关常量
+ */
+
+public interface GatewayConst {
+
+    String UNIQUE_ID = "uniqueId";
+
+    String DEFAULT_VERSION = "1.0.0";
+
+    String PROTOCOL_KEY = "protocol";
+
+}
