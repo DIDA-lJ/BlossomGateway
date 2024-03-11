ea3306b957df30c0aed3b8ac00738e6b2b4e582a
linqi <wkw4399xs@gmail.com>
2024-03-11 15:52:22 +0800

    feature:Http 服务执行器

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/config/HttpServiceInvoker.java
@@ -0,0 +1,11 @@
+package blossom.gateway.common.config;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description http协议的注册服务调用模型类
+ */
+
+public class HttpServiceInvoker extends AbstractServiceInvoker {
+
+}
