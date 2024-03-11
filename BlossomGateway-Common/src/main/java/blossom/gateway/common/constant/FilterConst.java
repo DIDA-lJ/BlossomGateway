aa474be32e2361580cef13afc7379f4b1ffc6b88
linqi <wkw4399xs@gmail.com>
2024-03-11 16:00:35 +0800

    feature:过滤器常量类

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/constant/FilterConst.java
@@ -0,0 +1,11 @@
+package blossom.gateway.common.constant;
+
+/**
+ * @InterfaceName FilterConst
+ * @Description 负载均衡常量类
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2024/03/11
+ */
+public interface FilterConst {
+}
