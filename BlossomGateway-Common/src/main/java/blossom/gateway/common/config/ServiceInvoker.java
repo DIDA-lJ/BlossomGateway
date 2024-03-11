229810da113016147989bd251de389d934e934ac
linqi <wkw4399xs@gmail.com>
2024-03-11 15:57:43 +0800

    feature:服务调用的接口模型描述

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/config/ServiceInvoker.java
@@ -0,0 +1,26 @@
+package blossom.gateway.common.config;
+
+/**
+ * @InterfaceName ServiceInvoker
+ * @Description 服务调用的接口模型描述
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2024/03/11
+ */
+public interface ServiceInvoker {
+
+    /**
+     * 获取真正的服务调用的全路径
+     */
+    String getInvokerPath();
+
+    void setInvokerPath(String invokerPath);
+
+    /**
+     * 获取该服务调用(方法)的超时时间
+     */
+    int getTimeout();
+
+    void setTimeout(int timeout);
+
+}
