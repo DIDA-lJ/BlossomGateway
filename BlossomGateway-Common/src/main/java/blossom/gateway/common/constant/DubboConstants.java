67084483ac977fa5c645cf8d8089adcff3bdeb41
linqi <wkw4399xs@gmail.com>
2024-03-11 16:00:19 +0800

    feature:Dubbo常量类

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/constant/DubboConstants.java
@@ -0,0 +1,20 @@
+package blossom.gateway.common.constant;
+
+/**
+ * @InterfaceName DubboConstants
+ * @Description Dubbo常量类
+ * @Version 1.0.0
+ * @Author LinQi
+ * @Date 2024/03/11
+ */
+public interface DubboConstants {
+
+    String DUBBO_PROTOCOL_PORT = "dubbo.protocol.port";
+
+    String DUBBO_APPLICATION_NAME = "dubbo.application.name";
+
+    String DUBBO_REGISTERY_ADDRESS = "dubbo.registery.address";
+
+    int DUBBO_TIMEOUT = 5000;
+
+}
