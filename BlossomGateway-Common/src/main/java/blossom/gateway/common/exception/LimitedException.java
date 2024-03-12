03b6ccf934e02c98f7ba5658939f943036b1f8e6
linqi <wkw4399xs@gmail.com>
2024-03-12 19:48:16 +0800

    feature:网关限流异常

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/exception/LimitedException.java
@@ -0,0 +1,23 @@
+package blossom.gateway.common.exception;
+
+
+import blossom.gateway.common.enums.ResponseCode;
+
+/**
+ * @author: linqi
+ * @description: 限流异常
+ */
+public class LimitedException  extends BaseException {
+
+ private static final long serialVersionUID = -5534700534739261461L;
+
+ public LimitedException(ResponseCode code) {
+  super(code.getMessage(), code);
+ }
+
+ public LimitedException(Throwable cause, ResponseCode code) {
+  super(code.getMessage(), cause, code);
+ }
+
+
+}
