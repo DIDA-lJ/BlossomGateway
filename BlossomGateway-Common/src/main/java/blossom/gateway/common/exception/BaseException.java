47800fd1d17740b6be1591119e97d771bdd4d777
linqi <wkw4399xs@gmail.com>
2024-03-12 19:44:37 +0800

    feature:基础异常

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/exception/BaseException.java
@@ -0,0 +1,45 @@
+package blossom.gateway.common.exception;
+
+import blossom.gateway.common.enums.ResponseCode;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 基础异常
+ */
+
+public class BaseException extends RuntimeException {
+
+    private static final long serialVersionUID = -5658789202563433456L;
+
+    public BaseException() {
+    }
+
+    protected ResponseCode code;
+
+    public BaseException(String message, ResponseCode code) {
+        super(message);
+        this.code = code;
+    }
+
+    public BaseException(String message, Throwable cause, ResponseCode code) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    public BaseException(ResponseCode code, Throwable cause) {
+        super(cause);
+        this.code = code;
+    }
+
+    public BaseException(String message, Throwable cause,
+                         boolean enableSuppression, boolean writableStackTrace, ResponseCode code) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.code = code;
+    }
+
+    public ResponseCode getCode() {
+        return code;
+    }
+
+}
