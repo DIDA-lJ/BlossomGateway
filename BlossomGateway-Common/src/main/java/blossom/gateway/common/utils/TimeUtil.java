95e58c7c37757716a40143486196e85cc02c5174
linqi <wkw4399xs@gmail.com>
2024-03-12 19:43:10 +0800

    feature:时间工具类

---
+++ BlossomGateway-Common/src/main/java/blossom/gateway/common/utils/TimeUtil.java
@@ -0,0 +1,40 @@
+package blossom.gateway.common.utils;
+
+import blossom.gateway.common.enums.ResponseCode;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 时间工具类
+ */
+
+public final class TimeUtil {
+
+    private static volatile long currentTimeMillis;
+
+    static {
+        currentTimeMillis = System.currentTimeMillis();
+        Thread daemon = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    currentTimeMillis = System.currentTimeMillis();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(1);
+                    } catch (Throwable e) {
+
+                    }
+                }
+            }
+        });
+        daemon.setDaemon(true);
+        daemon.setName("common-fd-time-tick-thread");
+        daemon.start();
+    }
+
+    public static long currentTimeMillis() {
+        return currentTimeMillis;
+    }
+}
