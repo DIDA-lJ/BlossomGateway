a0ee81a14174aa085b0152e3136ad0c302b55a59
linqi <wkw4399xs@gmail.com>
2024-03-14 19:29:06 +0800

    feature:Netty 服务

---
+++ BlossomGateway-Core/src/main/java/blossom/gateway/core/netty/NettyHttpClient.java
@@ -0,0 +1,65 @@
+package blossom.gateway.core.netty;
+
+import blossom.gateway.core.Config;
+import blossom.gateway.core.LifeCycle;
+import blossom.gateway.core.helper.AsyncHttpHelper;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.EventLoopGroup;
+import lombok.extern.slf4j.Slf4j;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+
+import java.io.IOException;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description
+ */
+
+@Slf4j
+public class NettyHttpClient implements LifeCycle {
+    private final Config config;
+
+    private final EventLoopGroup eventLoopGroupWoker;
+
+    private AsyncHttpClient asyncHttpClient;
+
+    public NettyHttpClient(Config config, EventLoopGroup eventLoopGroupWoker) {
+        this.config = config;
+        this.eventLoopGroupWoker = eventLoopGroupWoker;
+        init();
+    }
+
+    @Override
+    public void init() {
+        DefaultAsyncHttpClientConfig.Builder builder = new DefaultAsyncHttpClientConfig.Builder()
+                .setEventLoopGroup(eventLoopGroupWoker)
+                .setConnectTimeout(config.getHttpConnectTimeout())
+                .setRequestTimeout(config.getHttpRequestTimeout())
+                .setMaxRedirects(config.getHttpMaxRequestRetry())
+                .setAllocator(PooledByteBufAllocator.DEFAULT) //池化的byteBuf分配器，提升性能
+                .setCompressionEnforced(true)
+                .setMaxConnections(config.getHttpMaxConnections())
+                .setMaxConnectionsPerHost(config.getHttpConnectionsPerHost())
+                .setPooledConnectionIdleTimeout(config.getHttpPooledConnectionIdleTimeout());
+        this.asyncHttpClient = new DefaultAsyncHttpClient(builder.build());
+    }
+
+    @Override
+    public void start() {
+        AsyncHttpHelper.getInstance().initialized(asyncHttpClient);
+    }
+
+    @Override
+    public void shutdown() {
+        if (asyncHttpClient != null) {
+            try {
+                this.asyncHttpClient.close();
+            } catch (IOException e) {
+                log.error("NettyHttpClient shutdown error", e);
+            }
+        }
+    }
+}
