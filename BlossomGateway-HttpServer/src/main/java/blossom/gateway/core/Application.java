034d5eebae719729e5b1429488d17394b25036b6
linqi <wkw4399xs@gmail.com>
2024-03-20 23:32:02 +0800

    feature:网关服务 应用

---
+++ BlossomGateway-HttpServer/src/main/java/blossom/gateway/core/Application.java
@@ -0,0 +1,27 @@
+package blossom.gateway.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description
+ */
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/http-demo/ping")
+    public String ping() {
+        return "pong";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
