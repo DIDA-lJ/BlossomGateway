62bee3bb736ea94033a1d6adc92f0bace808953e
linqi <wkw4399xs@gmail.com>
2024-03-21 14:47:01 +0800

    feature:网关服务 基础异常

--- BlossomGateway-Common/src/main/java/blossom/project/common/exception/BaseException.java
+++ BlossomGateway-Common/src/main/java/blossom/project/common/exception/BaseException.java
@@ -1,6 +1,6 @@
-package blossom.gateway.common.exception;
+package blossom.project.common.exception;
 
-import blossom.gateway.common.enums.ResponseCode;
+import blossom.project.common.enums.ResponseCode;
 
 /**
  * @author linqi
