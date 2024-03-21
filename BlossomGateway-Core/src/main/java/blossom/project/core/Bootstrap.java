47358dd160d0a48322de5fca39bfd0d20c9f1dd8
linqi <wkw4399xs@gmail.com>
2024-03-21 14:48:48 +0800

    feature:网关服务 网关启动类

---
+++ BlossomGateway-Core/src/main/java/blossom/project/core/Bootstrap.java
@@ -0,0 +1,116 @@
+package blossom.project.core;
+
+import blossom.project.common.config.DynamicConfigManager;
+import blossom.project.common.config.ServiceDefinition;
+import blossom.project.common.config.ServiceInstance;
+import blossom.project.common.utils.NetUtils;
+import blossom.project.common.utils.TimeUtil;
+import blossom.project.config.center.api.ConfigCenter;
+import blossom.project.register.center.api.RegisterCenter;
+import blossom.project.register.center.api.RegisterCenterListener;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.Set;
+
+import static blossom.project.common.constant.BasicConst.COLON_SEPARATOR;
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 网关启动类
+ */
+@Slf4j
+public class Bootstrap
+{
+    public static void main( String[] args )
+    {
+        //加载网关核心静态配置
+        Config config = ConfigLoader.getInstance().load(args);
+        System.out.println(config.getPort());
+
+        //插件初始化
+        //配置中心管理器初始化，连接配置中心，监听配置的新增、修改、删除
+        ServiceLoader<ConfigCenter> serviceLoader = ServiceLoader.load(ConfigCenter.class);
+        final ConfigCenter configCenter = serviceLoader.findFirst().orElseThrow(() -> {
+            log.error("not found ConfigCenter impl");
+            return new RuntimeException("not found ConfigCenter impl");
+        });
+
+        // 从配置中心获取配置
+        configCenter.init(config.getRegistryAddress(), config.getEnv());
+        configCenter.subscribeRulesChange(rules -> DynamicConfigManager.getInstance()
+                .putAllRule(rules));
+
+
+        //启动容器
+        Container container = new Container(config);
+        container.start();
+
+        //连接注册中心，将注册中心的实例加载到本地
+        final RegisterCenter registerCenter = registerAndSubscribe(config);
+
+        //服务优雅关机
+        //收到kill信号时调用
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                registerCenter.deregister(buildGatewayServiceDefinition(config),
+                        buildGatewayServiceInstance(config));
+                container.shutdown();
+            }
+        });
+    }
+
+    private static RegisterCenter registerAndSubscribe(Config config) {
+        ServiceLoader<RegisterCenter> serviceLoader = ServiceLoader.load(RegisterCenter.class);
+        final RegisterCenter registerCenter = serviceLoader.findFirst().orElseThrow(() -> {
+            log.error("not found RegisterCenter impl");
+            return new RuntimeException("not found RegisterCenter impl");
+        });
+        registerCenter.init(config.getRegistryAddress(), config.getEnv());
+
+        //构造网关服务定义和服务实例
+        ServiceDefinition serviceDefinition = buildGatewayServiceDefinition(config);
+        ServiceInstance serviceInstance = buildGatewayServiceInstance(config);
+
+        //注册
+        registerCenter.register(serviceDefinition, serviceInstance);
+
+        //订阅
+        registerCenter.subscribeAllServices(new RegisterCenterListener() {
+            @Override
+            public void onChange(ServiceDefinition serviceDefinition, Set<ServiceInstance> serviceInstanceSet) {
+                log.info("refresh service and instance: {} {}", serviceDefinition.getUniqueId(),
+                        JSON.toJSON(serviceInstanceSet));
+                DynamicConfigManager manager = DynamicConfigManager.getInstance();
+                //将这次变更事件影响之后的服务实例再次添加到对应的服务实例集合
+                manager.addServiceInstance(serviceDefinition.getUniqueId(), serviceInstanceSet);
+                //修改发生对应的服务定义
+                manager.putServiceDefinition(serviceDefinition.getUniqueId(),serviceDefinition);
+            }
+        });
+        return registerCenter;
+    }
+
+    private static ServiceInstance buildGatewayServiceInstance(Config config) {
+        String localIp = NetUtils.getLocalIp();
+        int port = config.getPort();
+        ServiceInstance serviceInstance = new ServiceInstance();
+        serviceInstance.setServiceInstanceId(localIp + COLON_SEPARATOR + port);
+        serviceInstance.setIp(localIp);
+        serviceInstance.setPort(port);
+        serviceInstance.setRegisterTime(TimeUtil.currentTimeMillis());
+        return serviceInstance;
+    }
+
+    private static ServiceDefinition buildGatewayServiceDefinition(Config config) {
+        ServiceDefinition serviceDefinition = new ServiceDefinition();
+        serviceDefinition.setInvokerMap(Map.of());
+        serviceDefinition.setUniqueId(config.getApplicationName());
+        serviceDefinition.setServiceId(config.getApplicationName());
+        serviceDefinition.setEnvType(config.getEnv());
+        return serviceDefinition;
+    }
+}
